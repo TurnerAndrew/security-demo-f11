fff660818332bb64120e5b31361cb549de1d0471
Andrew Turner <turner.andrew0@gmail.com>
2021-10-21 11:11:12 -0500

    demo complete

---
+++ starter/index.js
@@ -0,0 +1,79 @@
+// const e = require("cors")
+
+const secretMessagesContainer = document.querySelector('#secret-messages-container')
+const secretMessageForm = document.querySelector('#secret-message-form')
+const getMessagesForm = document.querySelector('#get-messages-form')
+
+const baseURL = `http://localhost:4004/api/messages`
+
+const postMessage = body => axios.post(baseURL, body).then(res => {
+  console.log(res.data)
+  displayMessages(res.data)
+}).catch(err => {
+  console.log(err)
+  alert('Uh oh. not working....')
+})
+
+const getSecrets = (body) => {
+  axios.post(`${baseURL}/get`, body).then(res => {
+    console.log(res.data)
+    displaySecrets(res.data)
+  }).catch(err => console.log(err))
+}
+
+function submitHandler(e) {
+    e.preventDefault()
+
+    let pin = document.querySelector('#secret-message-pin-input')
+    let message = document.querySelector('#secret-message-input')
+
+    let bodyObj = {
+        pin: pin.value,
+        message: message.value
+    }
+
+    postMessage(bodyObj)
+
+    pin.value = ''
+    message.value = ''
+}
+
+function getHandler(e) {
+  e.preventDefault()
+
+  let pin = document.querySelector('#pin-input')
+
+  let bodyObj = {
+    pin: pin.value
+  }
+
+  getSecrets(bodyObj)
+
+  pin.value = ''
+}
+
+function createMessage(data) {
+    const messageCard = document.createElement('div')
+    messageCard.classList.add('message-card')
+
+    messageCard.innerHTML = `<p class="message">${data}</p>`
+
+    secretMessagesContainer.appendChild(messageCard)
+}
+
+function displayMessages(data) {
+  secretMessagesContainer.innerHTML = ``
+  for (let i = 0; i < data.messages.length; i++) {
+      createMessage(data.messages[i])
+  }
+}
+
+function displaySecrets(data) {
+  secretMessagesContainer.innerHTML = ``
+  for (let i = 0; i < data.length; i++){
+    createMessage(data[i])
+  }
+}
+
+secretMessageForm.addEventListener('submit', submitHandler)
+getMessagesForm.addEventListener('submit', getHandler)
